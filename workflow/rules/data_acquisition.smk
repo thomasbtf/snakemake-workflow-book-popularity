86ae9e65460c0bb8d746ca138c0db655be70e9c1
Thomas Battenfeld <thomas.battenfeld@gmail.com>
2021-03-12 09:32:59 +0100

    add rules for getting blurbs

--- workflow/rules/data_acquisition.smk
+++ workflow/rules/data_acquisition.smk
@@ -1,11 +1,28 @@
-rule extract_books_with_isbn:
+rule extract_records_with_isbn:
     input:
-        "",
+        "resources/bibliographic_data/{mrcfile}.mrc",
     output:
-        "",
+        xmls="data/raw-records/isbn-{mrcfile}.xml",
+        uris="data/URIs/_to-parse-from-{mrcfile}.tsv"
     log:
-        "logs/extract-books-with-isbn.log",
+        "logs/extract-records-with-isbn/{mrcfile}.log",
     conda:
         "../envs/pymarc.yaml"
     script:
-        "../scripts/extract-books-with-isbn.py"
+        "../scripts/extract-records-with-isbn.py"
+
+
+rule get_blurbs:
+    input:
+        "data/URIs/_to-parse-from-{mrcfile}.tsv",
+    output:
+        "data/blurbs/_parsed-blurbs-from-{mrcfile}.tsv",
+    params:
+        outdir=lambda w, output: os.path.dirname(output[0]),
+    threads: 4
+    log:
+        "logs/get-blurbs/{mrcfile}.log"
+    conda:
+        "../envs/beautifulsoup.yaml"
+    script:
+        "../scripts/get-blurbs.py"
---
+++ workflow/scripts/get-blurbs.py
@@ -0,0 +1,78 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed, thread
+from threading import BoundedSemaphore
+from datetime import datetime
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+
+import requests
+import csv
+import time
+
+
+def download_and_save_blurb(isbn, link, outdir):
+    try:
+        r = requests.get(link)
+        assert r.status_code == 200
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
+
+    soup = BeautifulSoup(r.content, "lxml")
+
+    outpath = f"{outdir}/{isbn}.txt"
+    with open(outpath, "w") as soup_writer:
+        soup_writer.write(soup.p.get_text())
+
+    return (isbn, outpath)
+
+
+def get_blurbs_to_download(path_to_link_csv, path_to_summary_file, outdir, max_threads):
+
+    # open csv file with links
+    print("Opening context managers", file=sys.stderr)
+    with open(path_to_link_csv, "r") as csv_file:
+
+        # create parser for csv file
+        print("Creating csv reader.", file=sys.stderr)
+        csv_reader = csv.reader(csv_file, delimiter="\t")
+
+        # start threads
+        print("Start submitting jobs.", file=sys.stderr)
+        futures = []
+        with ThreadPoolExecutor(max_workers=max_threads) as executor, open(path_to_summary_file, "w") as summary_writer:
+            for isbn, link in tqdm(csv_reader, mininterval = 1):
+                if "http://deposit.dnb.de/cgi-bin" in link:
+                    futures.append(executor.submit(download_and_save_blurb, isbn, link, outdir))
+
+            # save results as they are completed to the summary file
+            print("Finished submitting jobs. Waiting for results.", file=sys.stderr)
+
+            with tqdm(total=len(futures), mininterval=30) as pbar:
+                for cmpl_submit in as_completed(futures):
+
+                    if cmpl_submit.exception() is not None:
+                        raise cmpl_submit.exception()
+
+                    isbn, save_path = cmpl_submit.result()
+                    summary_writer.write(f"{isbn}\t{save_path}\n")
+                    pbar.update(1)
+
+
+if __name__ == "__main__":
+    sys.stderr = open(snakemake.log[0], "w")
+    max_threads = snakemake.threads
+
+    if max_threads < 1:
+        max_threads = 1
+
+    max_threads += 4
+
+    t = time.perf_counter()
+
+    get_blurbs_to_download(
+        snakemake.input[0],
+        snakemake.output[0],
+        snakemake.params.get("outdir", ""),
+        max_threads,
+    )
+
+    print("\nCode completed in {} sec(s).".format(time.perf_counter() - t), file=sys.stderr)
